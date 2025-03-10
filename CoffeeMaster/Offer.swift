2aa86f0c8093ac1ce1173d1f7c485c058e3274c9
Mark Paul <markpaulbusiness66@gmail.com>
2025-03-10 18:41:07 +0300

    added a color asset guide

---
+++ CoffeeMaster/Offer.swift
@@ -0,0 +1,18 @@
+//
+//  Offer.swift
+//  CoffeeMaster
+//
+//  Created by MARK on 10/03/2025.
+//
+
+import SwiftUI
+
+struct Offer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Offer()
+}
